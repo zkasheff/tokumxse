7b4d63a8365fef8e1a26ff9a33aa088f9a2f454d
Leif Walsh <leif.walsh@gmail.com>
2015-02-18 22:30:34 -0500

    fixed assert id

--- src/mongo/db/storage/kv/dictionary/kv_size_storer.cpp
+++ src/mongo/db/storage/kv/dictionary/kv_size_storer.cpp
@@ -211,7 +211,7 @@ namespace mongo {
                 Slice value(data.objdata(), data.objsize());
 
                 Status s = _metadataDict->insert(opCtx, key, value, false);
-                massert(28609, str::stream() << "KVSizeStorer::storeInto: insert: " << s.toString(), s.isOK());
+                massert(28619, str::stream() << "KVSizeStorer::storeInto: insert: " << s.toString(), s.isOK());
             }
             wuow.commit();
         } catch (WriteConflictException) {
